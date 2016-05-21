86a3f584a531d48f27a41837bb6500ceed59e496
brian avery <brian.avery@intel.com>
2016-05-20 19:07:56 -0700

    debug
    Signed-off-by: brian avery <brian.avery@intel.com>

--- scripts/pushToDockerhub.sh
+++ scripts/pushToDockerhub.sh
@@ -4,6 +4,8 @@ set -x
 # these are travis encrypted in the env section
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 echo "$TRAVIS_PULL_REQUEST = $TRAVIS_PULL_REQUEST"
+echo "{DOCKERHUB_REPO}\/ = ${DOCKERHUB_REPO}\/"
+docker images | grep ${DOCKERHUB_REPO}\/ | grep -v deps | grep -v \<none\> | awk '{print $1 ":" $2}'
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     # l8r also check $TRAVIS_REPO_SLUG so we only push to dockerhub on master
     docker images | grep ${DOCKERHUB_REPO}\/ | grep -v deps | grep -v \<none\> | awk '{print $1 ":" $2}' | xargs docker push
