e5c282400b4c9a273e75b37f7f812d41bf512be2
brian avery <brian.avery@intel.com>
2016-05-20 19:26:08 -0700

    idebug
    Signed-off-by: brian avery <brian.avery@intel.com>

--- scripts/pushToDockerhub.sh
+++ scripts/pushToDockerhub.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-set -e
-set -x
 # these are travis encrypted in the env section
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-echo "$TRAVIS_PULL_REQUEST = $TRAVIS_PULL_REQUEST"
-echo "{DOCKERHUB_REPO}\/ = ${DOCKERHUB_REPO}\/"
 docker images | grep ${DOCKERHUB_REPO}\/ | grep -v deps | grep -v \<none\> | awk '{print $1 ":" $2}'
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     # l8r also check $TRAVIS_REPO_SLUG so we only push to dockerhub on master
-    docker images | grep ${DOCKERHUB_REPO}\/ | grep -v deps | grep -v \<none\> | awk '{print $1 ":" $2}' | xargs docker push
+    for i in `docker images | grep ${DOCKERHUB_REPO}\/ | grep -v deps | grep -v \<none\> | awk '{print $1 ":" $2}'`; do
+	echo Pushing $i
+	docker push $i
+    done
 else
     print "No Pushy, TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
 
